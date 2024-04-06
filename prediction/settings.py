3049157a413f701fd945f14282cf127b7ef1cd09
Csedem0 <emmasobula@gmail.com>
2024-04-07 00:17:21 +0100

    2 commited

--- prediction/settings.py
+++ prediction/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "./accounts/static"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Default primary key field type
