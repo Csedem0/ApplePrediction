25543daa82a2eb4ac3a654fcbc1bf5847a93810e
Csedem0 <emmasobula@gmail.com>
2024-04-07 10:34:49 +0100

    static file 1

--- prediction/urls.py
+++ prediction/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/accounts/', permanent=False)),
@@ -26,4 +27,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
