447a15cce5a6fc6e76785b38098654bea10201cc
Csedem0 <emmasobula@gmail.com>
2024-04-07 12:00:42 +0100

    pls

--- prediction/urls.py
+++ prediction/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+#from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/accounts/', permanent=False)),
@@ -24,8 +27,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 ]
 
-# Add static files serving during development
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
